e9ab8b220202e416cfe7df2f46aa40c42e4a2175
Yash <121029653+yashrpawar@users.noreply.github.com>
2024-02-15 17:58:36 +0530

    added

---
+++ src/com/Emart/service/OrderService.java
@@ -0,0 +1,26 @@
+package com.Emart.service;
+
+import java.util.List;
+
+import com.Emart.beans.OrderBean;
+import com.Emart.beans.OrderDetails;
+import com.Emart.beans.TransactionBean;
+
+public interface OrderService {
+
+	public String paymentSuccess(String userName, double paidAmount);
+
+	public boolean addOrder(OrderBean order);
+
+	public boolean addTransaction(TransactionBean transaction);
+
+	public int countSoldItem(String prodId);
+
+	public List<OrderBean> getAllOrders();
+
+	public List<OrderBean> getOrdersByUserId(String emailId);
+
+	public List<OrderDetails> getAllOrderDetails(String userEmailId);
+
+	public String shipNow(String orderId, String prodId);
+}
---
+++ src/com/Emart/service/TransService.java
@@ -0,0 +1,6 @@
+package com.Emart.service;
+
+public interface TransService {
+
+	public String getUserId(String transId);
+}
